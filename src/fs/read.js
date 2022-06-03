9f39ffb3da9fc78f299938524ee7f0870a4946c8
Dima Litvinov <dmlit@ciklum.com>
2022-06-03 21:58:50 +0300

    feat/ add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,25 @@
+import { existsSync, readFileSync } from 'fs';
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from './utils/get-path.util.js';
+import { CustomValidationError } from '../errors/custom-validation-error.js';
+
 export const read = async () => {
-    // Write your code here 
+  const sourcePath = `${getPath(import.meta.url)}\\${FILES}/fileToRead.txt`;
+
+  try {
+    const isFilesExists = existsSync(sourcePath);
+
+    if (!isFilesExists) {
+      throw new CustomValidationError('FS operation failed');
+    }
+  } catch (err) {
+    throw err;
+  }
+
+  const file = readFileSync(sourcePath, 'utf-8');
+
+  process.stdout.write(file);
 };
+
+read();
