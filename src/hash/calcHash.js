067d26c418896ca10518b0255c82fba9289d480a
Dima Litvinov <dmlit@ciklum.com>
2022-06-04 01:11:59 +0300

    feat/add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+const {
+  createHash
+} = await import('crypto');
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from '../fs/utils/get-path.util.js';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const sourcePath = `${getPath(import.meta.url)}\\${FILES}\\fileToCalculateHashFor.txt`;
+  const fileBuffer = fs.readFileSync(sourcePath);
+  const hash = createHash('sha256');
+  hash.update(fileBuffer);
+
+  const hex = `${hash.digest('hex')}`;
+
+  return hex;
 };
+
+calculateHash();
