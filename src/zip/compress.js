766153ba1003e11bbec92bfd10bb52f22d35688a
Dima Litvinov <dmlit@ciklum.com>
2022-06-04 20:38:58 +0300

    feat/add zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from '../utils/get-path.util.js';
+
 export const compress = async () => {
-    // Write your code here 
+  const sourcePath = `${getPath(import.meta.url)}\\${FILES}\\fileToCompress.txt`;
+  const targetPath = `${getPath(import.meta.url)}\\${FILES}\\archive.gz`;
+  const stream = createReadStream(sourcePath);
+
+  stream.pipe(zlib.createGzip())
+    .pipe(createWriteStream(targetPath));
 };
+
+compress();
